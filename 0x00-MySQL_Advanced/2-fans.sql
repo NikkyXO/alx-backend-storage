f0ac520b092b950411f1b09c71eab20d5534e12d
NikkyXO <alexisdame2017@gmail.com>
2023-01-13 12:15:33 +0100

    fixed

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
 -- Write a SQL script that ranks country origins of bands,
 --ordered by the number of (non - unique) fans
 
-SELECT origin, SUM(fans) as no_fans FROM metal_bands
+SELECT origin, SUM(fans) AS no_fans
+FROM metal_bands
 GROUP BY origin
 ORDER BY no_fans DESC;
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,10 +2,12 @@
 -- main style, ranked by their longevity
 -- Column names must be: band_name and lifespan (in years)
 -- use attributes formed and split for computing the lifespan
+-- used TIMESTAMPDIFF() COALESCE(value, default). 
 
 SELECT
     band_name,
-    COALESCE(split, 2020) - formed as lifespan
+    TIMESTAMPDIFF(YEAR, CONCAT(formed, '-01-01'), 
+    CONCAT(split, '-01-01')) as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
